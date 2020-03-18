c237d20a141277a9caa3e4d7ea4bcfd144182617
liraUnik <liridonsejdiu88@gmail.com>
2020-03-18 17:25:13 +0100

    class based component first sample

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,41 @@
-import React from 'react';
+// import React from 'react';
 
 // const getYear = () => {
 //   const newDate = new Date();
 //   return newDate.getFullYear();
 // };
+// ! dynamic data
+// const user = {
+//   firsName: 'Lira',
+//   lastName: 'Sejdiu',
+//   age: 31
+// };
 
-const user = {
-  firsName: 'Lira',
-  lastName: 'Sejdiu',
-  age: 31
-};
+// const Header = () => {
+// return <div>The date is {getYear()}</div>;
+// ? dynamic data
+//   return (
+//     <div>
+//       <div> {user.firsName} </div>
+//       <div> {user.lastName} </div>
+//       <div> {user.age} </div>
+//     </div>
+//   );
+// };
 
-const Header = () => {
-  // return <div>The date is {getYear()}</div>;
-  return (
-    <div>
-      <div> {user.firsName} </div>
-      <div> {user.lastName} </div>
-      <div> {user.age} </div>
-    </div>
-  );
-};
+// export default Header;
+
+import React, { Component } from 'react';
+
+export class Header extends Component {
+  render() {
+    return (
+      <header>
+        <div className=''>Logo</div>
+        <input type='text' />
+      </header>
+    );
+  }
+}
 
 export default Header;

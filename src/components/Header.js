a33f92e995145f49460148e1e1d7a9af09c4a671
liraUnik <liridonsejdiu88@gmail.com>
2020-03-18 14:54:36 +0100

    dynamik data

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
+// const getYear = () => {
+//   const newDate = new Date();
+//   return newDate.getFullYear();
+// };
+
+const user = {
+  firsName: 'Lira',
+  lastName: 'Sejdiu',
+  age: 31
+};
+
 const Header = () => {
-  return <div>header</div>;
+  // return <div>The date is {getYear()}</div>;
+  return (
+    <div>
+      <div> {user.firsName} </div>
+      <div> {user.lastName} </div>
+      <div> {user.age} </div>
+    </div>
+  );
 };
 
 export default Header;

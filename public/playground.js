a50b9c15b30a5f7e071e6ed80977fb93f2dd9620
liraUnik <liridonsejdiu88@gmail.com>
2020-03-20 18:06:17 +0100

    classes palyground

---
+++ public/playground.js
@@ -0,0 +1,23 @@
+class Car {
+  constructor() {
+    this.wheels = 4;
+    this.type = 'Hatchback';
+  }
+}
+
+class React {
+  constructor() {
+    this.props = {};
+  }
+}
+
+class Ford extends Car {
+  constructor() {
+    super();
+    this.windows = 4;
+  }
+}
+
+const car = new Car();
+const ford = new Ford();
+console.log(ford.wheels);
